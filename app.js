6876bbfd39d35cc80dfb528627b3fb8e5ca76082
Pulkit Varshney <pulkit.varshney@office365.3pillarglobal.com>
2021-11-18 21:12:25 +0530

    Basic Master Setup complete

---
+++ app.js
@@ -0,0 +1,5 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
