8375fd599720b4486c60da4781c8d98743f7558c
Pulkit Varshney <pulkit.varshney@office365.3pillarglobal.com>
2021-11-22 10:05:49 +0530

    Final Commit

--- controllers/products.js
+++ controllers/products.js
@@ -52,6 +52,7 @@ const getAllProducts = async (req, res) => {
   }
 
   const page = Number(req.query.page) || 1;
+
   const limit = Number(req.query.limit) || 10;
   const skip = (page - 1) * limit;
 
